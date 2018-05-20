7ff44f8f6d7b5dce649808171c75a1b73e792f84
Pavel Belyakov <palb@yandex.ru>
2018-05-20 22:06:14 +0300

    DZ add 0518_4

--- task_generator_password.py
+++ task_generator_password.py
@@ -3,8 +3,7 @@ import string
 
 def password_generator(n):
     val = string.ascii_letters + string.digits + '!@#$%^&*()'
-    while 1:
-        passw = '{}'.format(''.join(map(lambda a:str(a), random.sample(val, n))))
-        yield passw
+    passw = '{}'.format(''.join(map(lambda a:str(a), random.sample(val, n))))
+    yield passw
 
 
