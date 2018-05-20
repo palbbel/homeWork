c9b405c85a025933fbe8dbf62aa757e5c5822864
Pavel Belyakov <palb@yandex.ru>
2018-05-20 19:24:27 +0300

    DZ 0518_3

--- task_generator_password.py
+++ task_generator_password.py
@@ -1,15 +1,10 @@
 import random
 import string
 
-
 def password_generator(n):
-    val = string.ascii_letters + string.digits + '!@#$%^&*()' #printable[:68]
-    while n:
-        passw = str(random.choice(val))
-        yield  passw
-        n -= 1
+    val = string.ascii_letters + string.digits + '!@#$%^&*()'
+    while 1:
+        passw = '{}'.format(''.join(map(lambda a:str(a), random.sample(val, n))))
+        yield passw
 
-if __name__ == '__main__':
-    n = int(input())
-    ''.join(i for i in password_generator(n))
 
